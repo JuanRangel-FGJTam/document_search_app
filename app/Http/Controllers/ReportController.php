b9d9d69672f6723585d74f84d7d39725c36ceea4
JhovanRM17 <jorge.rodriguezm@fgjtam.gob.mx>
2025-04-09 12:02:29 -0600

    Feat: Get Data with LAST_FOLIO_LEGACY

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -28,6 +28,7 @@ class ReportController extends Controller
 {
 
     protected $authApiService;
+    const LAST_FOLIO_LEGACY = 163191;
 
     public function __construct(AuthApiService $authApiService)
     {
@@ -295,7 +296,7 @@ class ReportController extends Controller
             "CONVERT(varchar(10), PGJ_EXTRAVIOS.FECHA_EXTRAVIO, 120) as fecha,
             COUNT(*) as total"
         )
-            ->join('PGJ_OBJETOS', 'PGJ_OBJETOS.ID_EXTRAVIO', '=', 'PGJ_EXTRAVIOS.ID_EXTRAVIO');
+            ->join('PGJ_OBJETOS', 'PGJ_OBJETOS.ID_EXTRAVIO', '=', 'PGJ_EXTRAVIOS.ID_EXTRAVIO')->where('PGJ_EXTRAVIOS.ID_EXTRAVIO' <= self::LAST_FOLIO_LEGACY);
 
         $queryMisplacements = Misplacement::selectRaw('DATE(misplacements.registration_date) as fecha, COUNT(*) as total')
             ->join('lost_documents as ld', 'misplacements.id', '=', 'ld.misplacement_id');
